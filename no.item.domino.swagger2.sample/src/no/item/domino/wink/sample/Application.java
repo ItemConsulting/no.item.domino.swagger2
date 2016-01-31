687192567364d3705b3e6c1736079e735efac371
rbrastad <runar.brastad@gmail.com>
2016-01-31 21:01:29 +0100

    init sample app petstore

---
+++ no.item.domino.swagger2.sample/src/no/item/domino/wink/sample/Application.java
@@ -0,0 +1,48 @@
+package no.item.domino.wink.sample;
+
+import io.swagger.jaxrs.config.BeanConfig;
+import io.swagger.sample.resource.PetResource;
+import io.swagger.sample.resource.PetStoreResource;
+import io.swagger.sample.resource.UserResource;
+
+import java.util.Set;
+
+import no.item.domino.swagger.wink.SwaggerApplication;
+
+public class Application extends SwaggerApplication {
+	
+	public Application() {
+		super( getSwaggerBeanConfiguration() );
+	}
+ 
+	@Override
+	public Set<Class<?>> getClasses() {
+
+		addResource(PetResource.class);
+		addResource(PetStoreResource.class);
+		addResource(UserResource.class);
+		
+		return getResources();
+	}
+	
+	
+	
+	private static BeanConfig getSwaggerBeanConfiguration(){ 
+				
+		BeanConfig cnfg = new BeanConfig();
+		cnfg.setVersion("");
+		cnfg.setSchemes(new String[]{"http","https"});
+		cnfg.setHost(""); 
+		cnfg.setBasePath("/sample/api/v2"); 
+		cnfg.setResourcePackage("io.swagger.sample.resource"); 
+		cnfg.setContact(""); 
+		cnfg.setTitle(""); 
+		cnfg.setDescription(""); 
+		cnfg.setScan(true); 
+		cnfg.setPrettyPrint(true); 
+		return cnfg;
+	}
+ 
+
+} 
+
---
+++ no.item.domino.swagger2.sample/src/no/item/domino/wink/sample/plugin/Activator.java
@@ -0,0 +1,52 @@
+package no.item.domino.wink.sample.plugin;
+import javax.ws.rs.ext.RuntimeDelegate;
+
+import org.apache.wink.common.internal.runtime.RuntimeDelegateImpl;
+import org.eclipse.core.runtime.Plugin;
+import org.osgi.framework.BundleContext;
+
+import no.item.domino.swagger.wink.util.Util;
+
+public class Activator extends Plugin {
+
+	private static BundleContext context;
+
+	static BundleContext getContext() {
+		return context;
+	}
+	
+	public Activator() { 
+	}
+
+	@Override
+	public void start(BundleContext context) throws Exception {
+		// Ensure that the service loader uses the right class
+		// See: http://www.mail-archive.com/discuss@restlet.tigris.org/msg07539.html
+		ClassLoader oldcl = Thread.currentThread().getContextClassLoader();
+		ClassLoader newcl = RuntimeDelegate.class.getClassLoader();
+		
+		Thread.currentThread().setContextClassLoader(newcl);
+		try {
+			RuntimeDelegate.setInstance(new RuntimeDelegateImpl());
+		} catch(Exception e){
+			e.printStackTrace();
+		} finally {
+			Thread.currentThread().setContextClassLoader(oldcl);
+		}
+		
+		super.start(context);
+	}
+	
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see
+	 * org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 */
+	@Override
+	public void stop(BundleContext bundleContext) throws Exception {
+		Util.debug("Stopping Swagger Test Plugin...", getClass().getName() ,"stop(BundleContext context)" );
+		Activator.context = null;
+	}
+	
+}
